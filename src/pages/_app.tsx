a50c2f5491564a56006e72344ff5d4a755b63e38
Ahmed Ghonim <ahmed.ghonim@g2k-group.com>
2023-04-21 03:09:39 +0200

    fix: 🚑️ yalaaaaa

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
-import '~/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "~/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Amplify } from "aws-amplify";
+import awsconfig from "../aws-exports";
+Amplify.configure({ ...awsconfig, ssr: true });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
