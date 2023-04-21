a2e5b8e65a799575e037f8a66917eb89bb283dfc
Ahmed Ghonim <ahmed.ghonim@g2k-group.com>
2023-04-21 03:21:54 +0200

    fix: 🚀 yala

---
+++ src/pages/add.tsx
@@ -0,0 +1,57 @@
+import { API, graphqlOperation } from "aws-amplify";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { createTodo } from "~/graphql/mutations";
+
+const AddTodo = () => {
+  const { push } = useRouter();
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    try {
+      const todo = { name, description };
+      await API.graphql(graphqlOperation(createTodo, { input: todo }));
+      setName("");
+      setDescription("");
+      push("/");
+    } catch (err) {
+      console.log("error creating todo:", err);
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 -mt-56 px-14 text-center">
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+        <h1 className="text-6xl font-bold">Add Todo</h1>
+
+        <form
+          className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center gap-8"
+          onSubmit={handleSubmit}
+        >
+          <input
+            className="border border-gray-400 p-2 rounded w-full"
+            type="text"
+            placeholder="Todo name"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+          <textarea
+            className="border border-gray-400 p-2 rounded w-full"
+            placeholder="Todo description"
+            onChange={(e) => setDescription(e.target.value)}
+            value={description}
+          />
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            type="submit"
+          >
+            Create Todo
+          </button>
+        </form>
+      </main>
+    </div>
+  );
+};
+
+export default AddTodo;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,22 @@ import { API, graphqlOperation } from "aws-amplify";
 import { GraphQLQuery } from "@aws-amplify/api";
 import { ListTodosQuery } from "~/API";
 import { listTodos } from "~/graphql/queries";
+import Link from "next/link";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home({ todos }: { todos: ListTodosQuery }) {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-6xl font-bold">Todos</h1>
+      {/* routing button to add page */}
+
+      <Link
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        href="/add"
+      >
+        Add Todo
+      </Link>
+
       <ul className="flex flex-col space-y-4">
         {todos.listTodos?.items?.map((todo) => (
           <li key={todo?.id} className="flex items-center space-x-4">
